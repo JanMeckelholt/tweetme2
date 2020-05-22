eecff0ee381e2869305a83c1b57e0faec6bdbde4
JanMeckelholt <Jan_Meckelholt@web.de>
2020-05-22 07:37:25 +0200

    JSON-Detail-View and template-homepage

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+
+from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>HelloWorld</h1>")
+    print(args, kwargs)
+    return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    print(args, kwargs)
+    data = {
+        "id": tweet_id,
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not found"
+        status = 404
+    return JsonResponse(data, status=status)
